d8c391901ba688a94c111f8952f8e88cea98a75c
alisalali <alis.alali@hotmail.com>
2024-08-08 21:32:18 +0300

    post table data with sorted method require to implement

--- components/posts/PostsTable.tsx
+++ components/posts/PostsTable.tsx
@@ -1,7 +1,73 @@
 import React from "react";
 
-const PostsTable = () => {
-  return <div>PostsTable</div>;
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+  TableCaption,
+} from "@/components/ui/table";
+
+import posts from "@/data/posts";
+import { Post } from "@/type/post";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+
+interface PostsTableProps {
+  limit?: number;
+  title?: string;
+}
+
+const tablePost = posts.map((post) => {
+  <TableRow key={post.id}>
+    <TableCell>{post.title}</TableCell>
+  </TableRow>;
+});
+
+const PostsTable = ({ limit, title }: PostsTableProps) => {
+  const sortedPosts: Post[] = [...posts].sort(
+    (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
+  );
+
+  return (
+    <div className="mt-10">
+      <h3 className="text-2xl mb-4 font-semibold">{title ? title : "Posts"}</h3>
+      <Table>
+        <TableCaption>A list of recent post</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Title</TableHead>
+            <TableHead className="hidden md:table-cell">Author</TableHead>
+            <TableHead className="hidden md:table-cell text-center">
+              Date
+            </TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {posts.map((post) => (
+            <TableRow key={post.id}>
+              <TableCell>{post.title}</TableCell>
+              <TableCell className="hidden md:table-cell">
+                {post.author}
+              </TableCell>
+              <TableCell className="hidden md:table-cell text-right">
+                {post.date}
+              </TableCell>
+              <TableCell>
+                <Link href={`/posts/edit/${post.id}`}>
+                  <button className="bg-blue-500 hover:bg-blue-700 py-2 px-4 text-white font-semiboldbold text-xs rounded">
+                    Edit
+                  </button>
+                </Link>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
+  );
 };
 
 export default PostsTable;
